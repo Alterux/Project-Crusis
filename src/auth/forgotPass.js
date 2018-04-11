f6e8de91ea246ddd32bc7e64f1141d9e19fc0d4a
unknown <andersmyt@gmail.com>
2018-04-11 14:50:59 +0200

    Forgotten password

--- src/auth/forgotPass.js
+++ src/auth/forgotPass.js
@@ -8,7 +8,15 @@ import { lang, en, no } from '../util/lang';
 
 class ForgotPass extends React.Component<{}> {
 
+  refs: {
+    inputForgotPassEmail: HTMLInputElement,
+    forgotPassButton: HTMLButtonElement,
+  }
+  errorEmail: boolean;
+
   render() {
+    let errorEmail = this.errorEmail;
+
     return (
       <div>
         <div id="title">
@@ -16,7 +24,13 @@ class ForgotPass extends React.Component<{}> {
           <div className="titleText"><h1>{lang.title}</h1></div>
         </div>
         <div className="inputForm">
-          {lang.forgotPassMsg}
+
+          <div className="forgotnPassHeadline"><h3>{lang.forgotPassHeadline}</h3></div><br></br>
+            <input className="form" placeholder="123@example.com" ref="inputForgotPassEmail" required></input>
+
+          <div className="forgotPassButton">
+            <button className="form" id="forgotPassButton" ref="forgotPassButton">{lang.forgotPassButtonText}</button>
+          </div>
         </div>
       </div>
     );
@@ -24,6 +38,38 @@ class ForgotPass extends React.Component<{}> {
 
   componentDidMount() {
 
+    if (this.refs.forgotPassButton) {
+      this.refs.forgotPassButton.onclick = () => {
+
+        //Get value
+        let forgotPassEmail = this.refs.inputForgotPassEmail.value;
+
+        //Validate
+        this.refs.inputForgotPassEmail.onchange = () => {validateEmail()};
+
+        if (this.errorEmail) {
+          console.log('Password reset failed');
+        } else {
+      // Kommer snart validateForgotPassEmail();
+        };
+      };
+    };
+
+    //Email format
+    const MAILFORMAT = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+
+    //Email validation
+    let validateEmail = () => {
+      let forgotPassEmail = this.refs.inputForgotPassEmail.value;
+      if (!forgotPassEmail || !forgotPassEmail.match(MAILFORMAT)) {
+        this.errorEmail = true;
+        this.refs.inputForgotPassEmail.setCustomValidity('invalid');
+      } else {
+        this.errorEmail = false;
+        this.refs.inputForgotPassEmail.setCustomValidity('');
+      };
+      this.forceUpdate();
+    };
   };
 };
 
