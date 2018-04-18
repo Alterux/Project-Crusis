2ac6378822addf0683910979c2349251655b4c21
Stian Jakobsen <alterux@gmail.com>
2018-04-18 17:32:49 +0200

    fixed error message in flow for imported packages

---
+++ flow-typed/npm/imported.js
@@ -0,0 +1,11 @@
+declare module 'jquery' {
+  declare module.exports: any;
+}
+
+declare module 'fullcalendar' {
+  declare module.exports: any;
+}
+
+declare module 'nodemailer' {
+  declare module.exports: any;
+}
