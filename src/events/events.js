67d31ea532135e72771dc36c72c1b54cd3f563ce
Stian Jakobsen <alterux@gmail.com>
2018-04-21 13:38:40 +0200

    added button instead of link for event creation

--- src/events/events.js
+++ src/events/events.js
@@ -2,6 +2,9 @@
 import * as React from 'react';
 import { Link, NavLink, HashRouter, Switch, Route } from 'react-router-dom';
 
+import createHashHistory from 'history/createHashHistory';
+const history = createHashHistory();
+
 import { lang, en, no } from '../util/lang';
 import { switchMonth } from '../util/modules';
 import { ErrorMessage, errorMessage } from '../util/errorMessage';
@@ -14,7 +17,8 @@ type State = {};
 
 class Events extends React.Component<Props, State> {
   refs: {
-    search: HTMLInputElement
+    search: HTMLInputElement,
+    createEventButton: HTMLButtonElement,
   };
 
   signedInUser = {};
@@ -45,7 +49,7 @@ class Events extends React.Component<Props, State> {
       table.push(
         <tr key={inputs.id}>
           <td><Link to={'/event/' + inputs.id}>{inputs.name ? inputs.name : lang.noName}</Link></td>
-          <td>{inputs.location}</td>
+          <td>{inputs.address}</td>
           <td>{inputs.city}</td>
           <td>{day}.{switchMonth(month)}.{year}</td>
           <td>{getDuration()}</td>
@@ -57,34 +61,35 @@ class Events extends React.Component<Props, State> {
       return (
         <div className='entry'>
           <h1>{lang.eventHeader}</h1>
-          <Link to="/event/create" className="button" > {lang.createEvent}</Link>
+          <button ref='createEventButton'>{lang.createEvent}</button>
         </div>
       );
     }
 
     return (
-      <div>
-        {this.signedInUser.userType > 1 ? createEvent() : null}
-        <div className='entry'>
-          <input ref='search' placeholder={lang.search}></input>
+      <div className='contentWrapper'>
+        <div className='textBoxWrapper'>
+          {this.signedInUser.userType > 1 ? createEvent() : null}
+          <div className='entry'>
+            <input ref='search' placeholder={lang.search}></input>
+          </div>
+          <div className="last entry eventmain">
+            <table className="eventlist">
+              <thead>
+                <tr>
+                  <th>{lang.tableName}</th>
+                  <th>{lang.tableLocation}</th>
+                  <th>{lang.tableCity}</th>
+                  <th>{lang.tableDate}</th>
+                  <th>{lang.tableDuration}</th>
+                </tr>
+              </thead>
+              <tbody>
+                {table}
+              </tbody>
+            </table>
+          </div>
         </div>
-        <div className="last entry eventmain">
-          <table className="eventlist">
-            <thead>
-              <tr>
-                <th>{lang.tableName}</th>
-                <th>{lang.tableLocation}</th>
-                <th>{lang.tableCity}</th>
-                <th>{lang.tableDate}</th>
-                <th>{lang.tableDuration}</th>
-              </tr>
-            </thead>
-            <tbody>
-              {table}
-            </tbody>
-          </table>
-        </div>
-
       </div>
     );
   }
@@ -122,6 +127,12 @@ class Events extends React.Component<Props, State> {
             this.events.push(event);
           }
           this.forceUpdate();
+          // create event button
+          if (this.refs.createEventButton) {
+            this.refs.createEventButton.onclick = () => {
+              history.push('/event/create/');
+            }
+          }
         }).catch((error: Error) => {
           if(errorMessage) console.log(error);
         });
