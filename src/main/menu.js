86dc7b2592228f9a9e06432d5948de50bfdc5491
Stian Jakobsen <alterux@gmail.com>
2018-04-21 13:39:26 +0200

    removed bottombar

--- src/main/menu.js
+++ src/main/menu.js
@@ -13,38 +13,68 @@ type Props = {};
 type State = {};
 
 class Menu extends React.Component<Props, State> {
+  refs: {
+    lang: HTMLInputElement,
+  }
+
   render() {
     let signedInUser = userService.getSignedInUser();
+    let langSelect = () => {
+      return(
+        <div id="langSelect">
+          <select name="lang" id="lang" ref="lang">
+            <option value="en">English</option>
+            <option value="no">Norsk</option>
+          </select>
+        </div>
+      );
+    }
+
     if(signedInUser) {
       return (
         <div id="menuBar" className="toolBar">
-          <div id="menuItems">
-            <NavLink activeStyle={{color: 'green'}} exact to='/'>{lang.home}</NavLink>{' '}
-            <NavLink activeStyle={{color: 'green'}} to={'/user/' + signedInUser.id}>{lang.myPage}</NavLink>{' '}
-            <NavLink activeStyle={{color: 'green'}} to='/events'>{lang.events}</NavLink>{' '}
-            <NavLink activeStyle={{color: 'green'}} to='/members'>{lang.members}</NavLink>{' '}
-          </div>
-          <div id="signOut">
-            <NavLink activeStyle={{color: 'green'}} to='/signout'>{lang.signOut}</NavLink>{' '}
+          <div className='menuWrapper'>
+            <div id="menuItems">
+              <NavLink activeStyle={{color: 'green'}} exact to='/'>{lang.home}</NavLink>{' '}
+              <NavLink activeStyle={{color: 'green'}} to={'/user/' + signedInUser.id}>{lang.myPage}</NavLink>{' '}
+              <NavLink activeStyle={{color: 'green'}} to='/events'>{lang.events}</NavLink>{' '}
+              <NavLink activeStyle={{color: 'green'}} to='/members'>{lang.members}</NavLink>{' '}
+            </div>
+            <div id="signOut">
+              <NavLink activeStyle={{color: 'green'}} to='/signout'>{lang.signOut}</NavLink>{' '}
+            </div>
+            {langSelect()}
           </div>
         </div>
       );
     }
     return (
-        <div id='signInBar' className="toolBar">
-          <div id="signIn">
-            <NavLink activeStyle={{color: 'green'}} to='/signin'>{lang.signIn}</NavLink>{' '}
-          </div>
-          <div id='signInHelp'>
-            <NavLink activeStyle={{color: 'green'}} to='/signup'>{lang.signUp}</NavLink>{' '}
-            <NavLink activeStyle={{color: 'green'}} to='/forgotpass'>{lang.forgotPass}</NavLink>{' '}
-          </div>
-        </div>
+      <div className='langSelectAuth'>
+        <select name="lang" id="lang" ref="lang">
+          <option value="en">English</option>
+          <option value="no">Norsk</option>
+        </select>
+      </div>
     );
   }
 
   componentDidMount() {
     menu = this;
+
+    // Get language or set language norwegian if none.
+    let currentLang = localStorage.getItem('lang');
+    if (currentLang) {
+      this.refs.lang.value = currentLang;
+    } else {
+      localStorage.setItem('lang', 'no');
+      window.location.reload();
+    }
+
+    // Set language onchange
+    this.refs.lang.onchange = () => {
+      localStorage.setItem('lang', this.refs.lang.value);
+      window.location.reload();
+    }
   }
 
   componentWillUnmount() {
@@ -53,48 +83,4 @@ class Menu extends React.Component<Props, State> {
 }
 let menu: ?Menu;
 
-class BottomBar extends React.Component<{}> {
-  refs: {
-    lang: HTMLSelectElement,
-    langButton: HTMLButtonElement,
-  }
-
-
-  render() {
-    return(
-      <div id="bottomBar">
-
-      {/* Language selection */}
-        <div id="langSelect">
-            <select name="lang" id="lang" ref="lang">
-              <option value="en">English</option>
-              <option value="no">Norsk</option>
-            </select>
-            <button ref='langButton' id="langButton">{lang.save}</button>
-        </div>
-      </div>
-    );
-  }
-
-  componentDidMount() {
-    // Get language or set language norwegian if none.
-    let cLang = localStorage.getItem('lang');
-    if (cLang) {
-      this.refs.lang.value = cLang
-    } else {
-      localStorage.setItem('lang', 'no');
-      window.location.reload();
-    }
-
-    // Set language onclick save.
-    this.refs.langButton.onclick = () => {
-      let sLang = this.refs.lang.value;
-      if (cLang !== sLang) {
-        localStorage.setItem('lang', sLang);
-        window.location.reload();
-      }
-    };
-  }
-}
-
-export { Menu, menu, BottomBar };
+export { Menu, menu };
